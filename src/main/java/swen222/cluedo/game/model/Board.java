b3af12bd08aec57d06ee7da0263f28d342105c25
Joseph Bennett <josephbennett42@gmail.com>
2015-07-26 22:05:17 +1200

    move location to its own class

--- src/main/java/swen222/cluedo/game/model/Board.java
+++ src/main/java/swen222/cluedo/game/model/Board.java
@@ -15,34 +15,6 @@ public class Board {
         // two tiles are adjacent if there is no wall between them
     }
 
-    static class Location  {
-        int x;
-        int y;
-
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Location location = (Location) o;
-
-            if (x != location.x) return false;
-            return y == location.y;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = x;
-            result = 31 * result + y;
-            return result;
-        }
-    }
-
     /**
      * Gets the tile at the given location.
      *
---
+++ src/main/java/swen222/cluedo/game/model/Location.java
@@ -0,0 +1,32 @@
+package swen222.cluedo.game.model;
+
+/**
+ * Created by josephbennett on 26/07/15.
+ */
+class Location {
+    int x;
+    int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (x != location.x) return false;
+        return y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+}
