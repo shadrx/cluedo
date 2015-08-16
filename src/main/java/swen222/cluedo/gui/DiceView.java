9abe6351daaef6b7b0d78b09f7bf87d2274b157c
Joseph Bennett <josephbennett42@gmail.com>
2015-08-16 15:40:06 +1200

    remove red background from dice view

--- src/main/java/swen222/cluedo/gui/DiceView.java
+++ src/main/java/swen222/cluedo/gui/DiceView.java
@@ -51,10 +51,6 @@ public class DiceView extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.red);
-        g.fillRect(0, 0, getWidth(), getHeight());
-        g.setColor(Color.black);
-
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
